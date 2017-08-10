719312073d3b76a22342cbae2f5b67dcb4a0a929
Andrea0229 <cpy123go@sina.cn>
2017-08-10 19:38:33 +0800

    task7

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -13,7 +13,7 @@ var main = require("../lib/main");
 describe("99 bottles of beer on the wall", function(){
     sinon.spy(console, 'log');
 
-    it("test1：测试返回所有歌词：", function(){
+    it("test1：测试返回所有歌词", function(){
 
         var result = main(99);
         var expect_string = getLyrics();
@@ -21,11 +21,9 @@ describe("99 bottles of beer on the wall", function(){
         expect(expect_string.join("")).to.equal(result.join(""));
     });
 
-    it("test2-测试函数中有返回值：", function(){
-
-        main();
-        var result = _.flatten(console.log.args).join("\n");
+    it("test2：测试空输入：", function(){
         var expect_string = '';
+        var result = main();
 
         expect(expect_string).to.equal(result);
     });
